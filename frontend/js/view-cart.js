7af63d9407806cf36bbd8df30d366ba60cc1eb98
ANITA-0604 <anita900604@gmail.com>
2024-12-10 20:08:42 -0500

    feat: add redirection after placing orders

--- frontend/js/view-cart.js
+++ frontend/js/view-cart.js
@@ -98,6 +98,7 @@ async function placeOrder(cartContent) {
         const result = await response.json();
         console.log('Order placed successfully:', result);
         alert('Order placed successfully!');
+        window.location.href='order-list.html'
     } catch (error) {
         console.error('Error placing order:', error);
         alert('Failed to place order. Please try again.');
